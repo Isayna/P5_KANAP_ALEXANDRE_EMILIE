c6d1615c6383ba429bf8830e01541a21ef80efbf
Alexandre Emilie <emilie.taillasson@me.com>
2022-05-01 13:51:51 +0200

    mise en place des élement de canapés de l'api sur la page d'accueil et ajout du href pour chaque produit

--- front/js/script.js
+++ front/js/script.js
@@ -3,10 +3,12 @@
 
 fetch ("http://localhost:3000/api/products")
 
-   .then(function(res){
+   .then(function(res) {
        const itemContainer = document.getElementById("items");
         res.json().then((data) => {
+            console.log(data)
             data.forEach((canap, index) => {
+                console.log(canap, index)
 
                 //CRÉATION DES ÉLÉMENTS HTML 
 
@@ -24,18 +26,24 @@ fetch ("http://localhost:3000/api/products")
                 setElementId(title, index);
                 setElementId(para, index);
 
-
+                //récuperation des images 
                 setImgData(img, canap.imageUrl, canap.altTxt);
 
+
+                //recupération du nom et de la description des produits
                 setElementClass(title, 'productName');
                 setElementClass(para, 'productDescription');
 
-                setAttribute(link, './product.html?id=42');
+                //ajout du lien vers la page produit 
+                link.setAttribute('href', `./product.html?id=${canap._id}`);
+                console.log('href', `./product.html?id=${canap._id}`)
 
-                               
+                //ajout du nom et de la description des canapés de manière dynamique               
                 title.innerHTML = canap.name;
                 para.innerHTML = canap.description;
 
+
+                //mise en place du conteneur "items"
                 itemContainer.appendChild(link);
                 link.appendChild(article);
                 article.appendChild(img);
@@ -46,24 +54,25 @@ fetch ("http://localhost:3000/api/products")
 
         });
    });
-
+    //fonction permettant d'ajouter un id à un élément HTML
    function setElementId(elem, index) {
        elem.id = `${elem.nodeName.toLowerCase()}${index}`
+       
    }
    
+   //fonction pour attribuer la source et la description de chaque image
    function setImgData(img, src, alt) {
        img.setAttribute('src', src);
        img.setAttribute('alt', alt);
+       
    }
-
+    //fonction permettant d'ajouter une class à un élément HTML (element fourni en paramètre)
    function setElementClass(elem, value) {
        elem.classList.add(value);
+       console.log(value)
    }
    
 
- function setAttribute(attribute, value) {
-     attribute.setAttribute('href', value);
- }
 
   
    
